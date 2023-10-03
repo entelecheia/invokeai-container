c73c69356d0d5449a7bf1ff816613538223f3348
Young Joon Lee <entelecheia@hotmail.com>
2023-10-04 07:54:11 +0900

    refactor(docker): remove app cloning scripts, add web server launch command

--- .docker/scripts/launch.sh
+++ .docker/scripts/launch.sh
@@ -2,17 +2,5 @@
 # add your custom commands here that should be executed every time the docker container starts
 echo "Starting docker container..."
 
-# Clones the app repository from GitHub
-if [[ "${VARIANT}" == "app" ]]; then
-    if [ ! -d "${APP_INSTALL_ROOT}/${APP_CLONE_DIRNAME}" ]; then
-        # clone app repo
-        echo "Cloning app repo"
-        echo "from ${APP_SOURCE_REPO} to ${APP_INSTALL_ROOT}/${APP_CLONE_DIRNAME}"
-        git clone "https://github.com/${APP_SOURCE_REPO}.git" "$APP_INSTALL_ROOT/${APP_CLONE_DIRNAME}"
-    else
-        echo "App repo already cloned"
-    fi
-    # install app dependencies
-    # pip install -r $APP_INSTALL_ROOT/$APP_CLONE_DIRNAME/requirements.txt
-fi
-
+# Launch the web server (do it every time you run InvokeAI):
+invokeai-web
