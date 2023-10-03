cdbcb424d98e68f9c86dd62cf27a052b053a7cef
Young Joon Lee <entelecheia@hotmail.com>
2023-10-04 07:55:47 +0900

    chore(docker):  duplicate command in configure.sh (https://github.com/invoke-ai/InvokeAI/issues/4734)

--- .docker/scripts/configure.sh
+++ .docker/scripts/configure.sh
@@ -8,4 +8,3 @@ invokeai-configure --root ./invokeai
 
 # Configure InvokeAI and install a starting set of image generation models (you only need to do this once):
 invokeai-configure --root ./invokeai
-
